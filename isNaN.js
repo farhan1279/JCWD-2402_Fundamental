afcc914294127657c319a9dc26748dabcb82c1db
masdefry <muhammaddefryan@gmail.com>
2023-03-10 09:18:48 +0700

    Day-08 Done

---
+++ isNaN.js
@@ -0,0 +1,7 @@
+// isNaN
+// Digunakan untuk mengecek value berupa number atau bukan
+// Inputannya number -> false
+// Inputannya character -> true
+
+console.log(isNaN(123)) // false
+console.log(isNaN('abc')) // true
